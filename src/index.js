ae29356dc1050df8a729f40c60543030899490bd
Greg Schmitt <gregjschmitt@gmail.com>
2021-12-07 10:39:36 -0500

    commit tree 4c6440d528615d1d3807f35d4333fa630ccff5db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+  form = document.querySelector("form");
+  form.addEventListener("submit", (e) => { 
+    e.preventDefault()
+    const id = e.target.searchByID.value;
+    console.log("ID: ", id);
+    form.reset();
+    fetchURL = `http://localhost:3000/movies/${id}`
+    console.log("fetchURL: ", fetchURL);
+    fetch(fetchURL)
+    .then((resp) => resp.json())
+    .then((json) => renderMovie(json))
+  });
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+function renderMovie(movie) {
+  console.log("MOVIE: ", movie);
+  h4 = document.querySelector("h4");
+  console.log("h4: ", h4);
+  h4.textContent = movie.title;
+  p = document.querySelector("p");
+  p.textContent = movie.summary;
+  
+}
